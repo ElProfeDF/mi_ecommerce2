e864ff8a4df9f50efff88169b6749d5a911793e8
ElProfeDF <pablo.defilippis@gmail.com>
2025-12-01 06:59:12 -0300

    Act3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/mi_ecommerce2/", 
+  base: "mi_ecommerce2", 
 })
